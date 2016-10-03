f627530a8960a35fd97b9ca357070cc4922f6973
Ryan Biwer <ryan@brightcellars.com>
2016-10-03 16:23:34 -0400

    API request?

--- src/event.js
+++ src/event.js
@@ -1,17 +1,50 @@
-function getAuthUrl() {
-    let clientId = "40658f2a8e1bdba55e45";
-    let scope = "repo";
-    let queryString = `client_id=${clientId}&scope=${scope}`;
-    return `https://github.com/login/oauth/authorize?${queryString}`;
-}
+const $        = require("jquery"),
+      urlParse = require("url-parse");
 
-chrome.identity.launchWebAuthFlow(
-    {
-        url: getAuthUrl(),
-        interactive: true
-    },
+$
+    .ajax({
+        url: "https://api.github.com/repos/ryb73/upgraded-waffle/pulls/1",
+        dataType: "json"
+    })
+    .then((result) => {
+        console.log(result);
+    })
+    .fail((err) => {
+        console.error(err);
+    })
+    .done();
 
-    (redirect_url) => {
-        alert(redirect_url);
-    }
-);
+// function getAuthUrl() {
+//     let clientId = "40658f2a8e1bdba55e45";
+//     let scope = "repo";
+//     let queryString = `client_id=${clientId}&scope=${scope}`;
+//     return `https://github.com/login/oauth/authorize?${queryString}`;
+// }
+
+// chrome.identity.launchWebAuthFlow(
+//     {
+//         url: getAuthUrl(),
+//         interactive: true
+//     },
+
+//     (redirectUrl) => {
+//         if(!redirectUrl)
+//             return;
+
+//         let parsedUrl = urlParse(redirectUrl, null, true);
+//         let secret = parsedUrl.query.code;
+
+//         $
+//             .ajax({
+//                 url: "https://api.github.com/repos/ryb73/upgraded-waffle/pulls/1",
+//                 dataType: "json"
+//             })
+//             .then((result) => {
+//                 console.log(result);
+//             })
+//             .fail((err) => {
+//                 console.error(err);
+//             })
+//             .done();
+//     }
+// );
